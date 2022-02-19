4378fa44a2ef0d9c3428278c1ecd0fa8b53dea9a
jaksehsekhja <jaksehsekhja@gmail.com>
2022-02-19 16:04:32 +0300

    simple QR_code generator and Scanner

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import  pyqrcode
 import webbrowser
 from pyzbar.pyzbar import decode
 from PIL import Image
-
-qr = pyqrcode.create("https://github.com/jaksehsekhja/flashlight-app")
+print("you can check the QRcode image from the file 'myCode.png'")
+inuser=input("enter the link to the site you want ::  ")
+#qr = pyqrcode.create("https://github.com/jaksehsekhja/flashlight-app")
+qr=pyqrcode.create(inuser)
 qr.png("myCode.png",scale=8)
-
-d=decode(Image.open("myCode.png"))
-url=d[0].data.decode("ascii")
-webbrowser.open(url)
+iu=input("do you want to open the link y/n for yes or no  ::  ")
+if(iu=='y'):
+    d=decode(Image.open("myCode.png"))
+    url=d[0].data.decode("ascii")
+    webbrowser.open(url)
+else:
+    print("have a lovely day ")
